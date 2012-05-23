a53a851336d3dfa417d35e4d92e99a7eb1d4591b
Sean Sorrell <ssorrell@squareup.com>
2012-05-23 14:04:18 -0400

    hardcode DISPLAY so we can run selenium on linux

--- lib/kochiku/build_strategies/build_all_strategy.rb
+++ lib/kochiku/build_strategies/build_all_strategy.rb
@@ -3,6 +3,7 @@ module BuildStrategy
     def execute_build(build_kind, test_files)
       system "env -i HOME=$HOME"+
       " PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin"+
+      " DISPLAY=localhost:0.1" +
       " TEST_RUNNER=#{build_kind}"+
       " RUN_LIST=#{test_files.join(',')}"+
       " bash --noprofile --norc -c 'ruby -v ; source ~/.rvm/scripts/rvm ; source .rvmrc ; mkdir log ; script/ci worker 2>log/stderr.log 1>log/stdout.log'"
