fc44bf4d151848353132587e57f8cb58eb141da4
Nolan Evans and Rob Olson <github+nolan+rob@squareup.com>
2012-06-19 12:10:18 -0700

    Remove default timeout from execute_with_timeout

--- lib/kochiku/build_strategies/build_all_strategy.rb
+++ lib/kochiku/build_strategies/build_all_strategy.rb
@@ -1,18 +1,21 @@
 module BuildStrategy
   class BuildAllStrategy
     def execute_build(build_kind, test_files)
-      execute_with_timeout "env -i HOME=$HOME"+
-      " PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin"+
-      " TEST_RUNNER=#{build_kind}"+
-      " RUN_LIST=#{test_files.join(',')}"+
-      " bash --noprofile --norc -c 'ruby -v ; source ~/.rvm/scripts/rvm ; source .rvmrc ; mkdir log ; script/ci worker 2>log/stderr.log 1>log/stdout.log'"
+      execute_with_timeout(
+          "env -i HOME=$HOME"+
+          " PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin"+
+          " TEST_RUNNER=#{build_kind}"+
+          " RUN_LIST=#{test_files.join(',')}"+
+          " bash --noprofile --norc -c 'ruby -v ; source ~/.rvm/scripts/rvm ; source .rvmrc ; mkdir log ; script/ci worker 2>log/stderr.log 1>log/stdout.log'",
+        60 * 60     # 1 hour
+      )
     end
 
     def artifacts_glob
       ['log/*log', 'spec/reports/*.xml', 'features/reports/*.xml']
     end
 
-    def execute_with_timeout(command, timeout = 60 * 60)
+    def execute_with_timeout(command, timeout)
       pid = Process.spawn(command)
       begin
         Timeout.timeout(timeout) do
