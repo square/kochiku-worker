1ece977342cc1229013ede79877258d276df31fd
Matthew Todd and Rafael Ferrer <github+matthewtodd+rafael@squareup.com>
2013-08-13 12:07:48 -0700

    Pass $USER along to the ci command.
    This allows `sq migrate` to detect the appropriate user when connecting
    to postgres datasources.

--- lib/kochiku/build_strategies/build_all_strategy.rb
+++ lib/kochiku/build_strategies/build_all_strategy.rb
@@ -91,6 +91,7 @@ module BuildStrategy
       end
       (
         "env -i HOME=$HOME" +
+        " USER=$USER" +
         " PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/share/python:$M2" +
         " DISPLAY=localhost:1.0" +
         " TEST_RUNNER=#{build_kind}" +
