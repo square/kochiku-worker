1d2b5aa87e9f1d6a9a9ccc648b370e1bcd8510c6
Jim Kingdon <kingdon@panix.com>
2014-02-05 07:16:00 -0800

    Fix up block to and_return.
    This was not automatically converted by transpec.

--- spec/kochiku/build_strategies/build_all_strategy_spec.rb
+++ spec/kochiku/build_strategies/build_all_strategy_spec.rb
@@ -6,9 +6,9 @@ describe BuildStrategy::BuildAllStrategy do
 
   before do
     old_spawn = Process.method(:spawn)
-    allow(Process).to receive(:spawn).and_return do |*args|
+    allow(Process).to receive(:spawn).and_return { |*args|
       @spawned_pid = old_spawn.call(*args)
-    end
+    }
     File.unlink(BuildStrategy::BuildAllStrategy::LOG_FILE) if File.exists?(BuildStrategy::BuildAllStrategy::LOG_FILE)
   end
 
