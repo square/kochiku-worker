0856ee427fa4342930703e5afaa65a122e415654
Alan Si <ysi@squareup.com>
2016-03-21 14:31:51 -0700

    CII-702 Fix execute_build method parameter bug
    The method in lib/kochiku/build_strategies/log_and_random_fail_strategy.rb should accept build_id as first argument

--- lib/kochiku/build_strategies/log_and_random_fail_strategy.rb
+++ lib/kochiku/build_strategies/log_and_random_fail_strategy.rb
@@ -1,6 +1,6 @@
 module BuildStrategy
   class LogAndRandomFailStrategy
-    def execute_build(build_kind, test_files, test_command, timeout, options)
+    def execute_build(build_attempt_id, build_kind, test_files, test_command, timeout, options)
       system %[ruby -e "now = Time.now.usec; File.open('now.log', 'w') {|f|f.write(now)}; exit(now % 3 == 0 ? 1 : 0)"]
     end
 
